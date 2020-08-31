12e3b8055bc69495955fded3138c6a9f3feeefe0
sukill <ckdrms622@gmail.com>
2020-08-31 23:27:23 +0900

    change mcap path

--- scripts/load.sh
+++ scripts/load.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-sudo ~/.mbpu/bin/mcap -x 0x1818 -p ~/.mbpu/bin/MDL_TOP_tandem2.bit
+sudo /opt/embeddedsw/mcap/linux/mcap -x 0x1818 -p ~/.mbpu/bin/MDL_TOP_tandem2.bit
 
 sudo insmod ~/.mbpu/mdlx.ko poll_mode=1
 sleep 3
